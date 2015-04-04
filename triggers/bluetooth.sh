76578cbf3590d39c9e8441e8a077b1922b269636
HokieGeek <andres.f.perez@gmail.com>
2015-04-03 21:36:54 -0400

    Removed extra stuff from bluetooth trigger handler

--- triggers/bluetooth.sh
+++ triggers/bluetooth.sh
@@ -5,5 +5,4 @@ here=$(cd `dirname $0`; pwd)
 osdnotifier=${here}/../notifiers/osdnotifier/osdnotifier.py
 
 ${osdnotifier} --content '${image '${here}'/bt.png}' \
-               --color-fg "#0a3d91" \
                --alignment "bottom_right"
