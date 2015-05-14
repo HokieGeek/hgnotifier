85d8ffd0e7e165fbf991b743cf2429a4b2ae53ce
HokieGeek <andres.f.perez@gmail.com>
2015-05-13 21:48:58 -0400

    Stuff y cosas

---
+++ notifiers/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+from PIL import Image
+image = Image.open('/home/andres/src/system-notifier/triggers/bt.png')
+image.show()
--- system-notifier.py
+++ system-notifier.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
 
 import subprocess
+import os
 
-subprocess.call("/home/andres/src/system-notifier/triggers/bluetooth.sh", shell=True)
+log = open("/tmp/system-notifier.log", 'w')
+# log.write(os.environ)
+os.environ['HOME'] = '/home/andres'
+log.write(os.environ['HOME'])
+log.close()
+
+subprocess.call("/home/andres/src/system-notifier/triggers/bluetooth.sh", shell=True, env=os.environ)
 
 #/etc/udev/rules.d/51-bluetooth.rules
 # SUBSYSTEM=="rfkill", ATTR{type}=="bluetooth", ATTR{state}=="1" RUN+="/usr/local/bin/system-notifier bluetooth enabled"
