4329841955ab39c0ae6d4271d7cc69792ba0e660
HokieGeek <andres.f.perez@gmail.com>
2015-05-14 08:33:19 -0400

    Being smarter about things

--- notifiers/capslock.sh
+++ notifiers/capslock.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+id="hgnotifier_capslock"
+
 case $1 in
     --show)
-        echo "A" | dzen2 -p -title-name 'osdnotifier_caps' \
+        echo "A" | dzen2 -p -title-name ${id} \
                             -fn '-*-terminus-bold-r-*-*-15-*-*-*-*-*-*-*' \
                             -fg "#005f00" -bg "#000000" \
                             -w 20 -y -30 -x -35 \
                              -e 'raise'&
-        sleep .1s && transset-df --name 'osdnotifier_caps' .65 >/dev/null 2>&1
+        sleep .1s && transset-df --name ${id} .65 >/dev/null 2>&1
         ;;
-    --hide) pkill -f osdnotifier_caps ;;
+    --hide) pkill -f ${id} ;;
 esac
