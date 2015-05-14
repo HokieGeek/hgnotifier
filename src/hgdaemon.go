1ab18cd00801e30fd8b7e86b381bb0488709f8c3
HokieGeek <andres.f.perez@gmail.com>
2015-05-14 19:35:46 -0400

    Having a harder time with this daemon than I thought I would, tbh

--- src/hgdaemon.go
+++ src/hgdaemon.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	"hgnotify"
+    "hgnotify"
 )
 
 func main() {
-	// TODO: this is currently broken as these functions block.
-	// I need for the control listener to communicate back with this dude
-	hgnotify.StartDataListener(7777)
-	hgnotify.StartControlListener(7778)
+    // TODO: this is currently broken as these functions block.
+    // I need for the control listener to communicate back with this dude
+    stop := false
+
+    // TODO: figure out how to use channels to stop
+    hgnotify.StartDataListener(7777)
+    // hgnotify.StartControlListener(7778)
+
+    for {
+        if stop {
+            return
+        }
+    }
 }
