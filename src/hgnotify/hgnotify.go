3b3e9a2ba5610b05d25b50c300ba31662926a152
HokieGeek <andres.f.perez@gmail.com>
2015-05-14 19:36:44 -0400

    Fixed tabs again

--- src/hgnotify/hgnotify.go
+++ src/hgnotify/hgnotify.go
@@ -1,34 +1,34 @@
 package hgnotify
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 type Header struct {
-	Timestamp time.Time
+    Timestamp time.Time
 }
 
 type Msg struct {
-	Hdr     Header
-	Payload string
+    Hdr     Header
+    Payload string
 }
 
 type HgNotify int
 
 func (t *HgNotify) SendMsg(msg *Msg, reply *int) error {
-	fmt.Println("SendMsg(", *msg, ")")
-	return nil
+    fmt.Println("SendMsg(", *msg, ")")
+    return nil
 }
 
 type HgNotifyCtrl int
 
 func (t *HgNotifyCtrl) Stop(msg *Msg, reply *int) error {
-	fmt.Println("Stop(", *msg, ")")
-	return nil
+    fmt.Println("Stop(", *msg, ")")
+    return nil
 }
 
 func (t *HgNotifyCtrl) Restart(msg *Msg, reply *int) error {
-	fmt.Println("Restart(", *msg, ")")
-	return nil
+    fmt.Println("Restart(", *msg, ")")
+    return nil
 }
